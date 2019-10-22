e77356c3f04bb3699b168154090f9506487cd3f7
amandamarkmann <amanda.markmann@live.se>
2019-10-22 10:10:17 +0200

    First commit

---
+++ booklender/src/test/java/se/lexicon/amanda/booklender/BooklenderApplicationTests.java
@@ -0,0 +1,12 @@
+package se.lexicon.amanda.booklender;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklenderApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
