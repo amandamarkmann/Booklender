98902658c17b1e5d9e936aba50c95b323a47a064
amandamarkmann <amanda.markmann@live.se>
2019-11-13 15:04:29 +0100

    Completed LibraryUserRepoTest

--- booklender/src/test/java/se/lexicon/amanda/booklender/test/data/LibraryUserRepoTest.java
+++ booklender/src/test/java/se/lexicon/amanda/booklender/test/data/LibraryUserRepoTest.java
@@ -4,6 +4,7 @@ package se.lexicon.amanda.booklender.test.data;
 import static org.junit.Assert.*;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -25,6 +26,8 @@ public class LibraryUserRepoTest {
 	@Autowired
 	private LibraryUserRepo testObject;
 	private LibraryUser testUser;
+	private int userId;
+	private String email;
 
 	
 	@Before
@@ -32,6 +35,8 @@ public class LibraryUserRepoTest {
 	
 		testUser = new LibraryUser(LocalDate.parse("2019-11-11"), "Test Testsson", "test@test.se");
 		testUser = testObject.save(testUser);
+		userId = testUser.getUserId();
+		email = testUser.getEmail();
 	}
 	
 	
@@ -43,6 +48,33 @@ public class LibraryUserRepoTest {
 		assertEquals(LocalDate.parse("2019-11-11"), testUser.getRegDate());
 		assertEquals("Test Testsson", testUser.getName());
 		assertEquals("test@test.se", testUser.getEmail());
+	}
+	
+	@Test
+	public void test_findAll() {
+		assertNotNull(testObject.findAll());
+	}
+	
+	@Test
+	public void test_findById_userId() {
+		Optional<LibraryUser> result = testObject.findByUserId(userId);
+		assertTrue(result.isPresent());
+		assertEquals(userId, result.get().getUserId());
+}
+	
+	
+	@Test
+	public void test_delete_by_userId() {
+		assertTrue(testObject.findByUserId(userId).isPresent());
+		testObject.deleteById(userId);
+		assertFalse(testObject.findByUserId(userId).isPresent());
+	}
+	
+	@Test
+	public void test_findByEmail() {
+		Optional<LibraryUser> result = testObject.findByEmail(email);
+		assertTrue(result.isPresent());
+		assertEquals(email, result.get().getEmail());
 	}
 	
 }
